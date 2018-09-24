eeb6ca20b250213a0593891138c38004f6a470fa
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-09-24 20:43:58 +0700

    Add statement no.

--- source-code/src/Triangle.php
+++ source-code/src/Triangle.php
@@ -12,27 +12,27 @@ class Triangle
      */
     public function validateTriangle($a, $b, $c)
     {
-        $isATriangle = False;
+        $isATriangle = False;                                       # 1
 
-        if (($a < $b + $c) && ($b < $a + $c) && ($c < $a + $b)) {
-            $isATriangle = True;
-        }
+        if (($a < $b + $c) && ($b < $a + $c) && ($c < $a + $b)) {   # 2
+            $isATriangle = True;                                    # 3
+        }                                                           # 4
 
-        if ($isATriangle) {
+        if ($isATriangle) {                                         # 5
 
-            if ($a == $b && $b == $c) {
-                return "Equilateral";
+            if ($a == $b && $b == $c) {                             # 6
+                return "Equilateral";                               # 7
 
-            } else if ($a != $b && $a != $c && $b != $c) {
-                return "Scalene";
+            } else if ($a != $b && $a != $c && $b != $c) {          # 8
+                return "Scalene";                                   # 9
 
-            } else {
-                return "Isosceles";
-            }
+            } else {                                                # 10
+                return "Isosceles";                                 # 11
+            }                                                       # 12
 
-        } else {
-            return "Not a Triangle";
-        }
+        } else {                                                    # 13
+            return "Not a Triangle";                                # 14
+        }                                                           # 15
 
     }
 }
